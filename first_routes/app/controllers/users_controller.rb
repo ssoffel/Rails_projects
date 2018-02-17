0781794f0e62140c98ea7f6aa04aa3e5a199d58f
mlaborde45 <mlaborde@bonbiniconsulting.com>
2018-02-17 08:35:41 -0500

    add first routes project

---
+++ first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def index
+    user = User.all
+    render json: user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: params
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
